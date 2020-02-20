9fcc1420b117d1ded4536474af8fdd6d94391b8e
Richard Schaubeck <richard.schaubeck@gmail.com>
2020-02-19 22:49:13 -0500

    created basic rails application

---
+++ W5D4/demo_project/app/models/House.rb
@@ -0,0 +1,11 @@
+class House < ApplicationRecordhouse
+
+    validates :address, presence: true
+
+    has_many(:residents,
+        primary_key: :id,
+        foreign_key: :house_id,
+        class_name: :Person
+    )
+
+end
